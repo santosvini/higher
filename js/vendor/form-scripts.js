a9501570373d1995b49f751be2cbdd4fd8e372e4
MarcusJacob <97745703+MarcusJacob@users.noreply.github.com>
2022-02-19 23:32:41 -0300

    UPDATE-3

--- js/vendor/form-scripts.js
+++ js/vendor/form-scripts.js
@@ -2,7 +2,7 @@ $("#contactForm").validator().on("submit", function (event) {
     if (event.isDefaultPrevented()) {
         // handle the invalid form...
         formError();
-        submitMSG(false, "Did you fill in the form properly?");
+        submitMSG(false, "Você preencheu o formulário corretamente?");
     } else {
         // everything looks good!
         event.preventDefault();
